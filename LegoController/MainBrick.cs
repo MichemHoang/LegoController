84603db7da7e9069253534479701ee92db0fcf1a
Michem <Hoang999.matrix@gmail.com>
2017-06-27 09:50:42 +0200

    finalV

--- LegoController/MainBrick.cs
+++ LegoController/MainBrick.cs
@@ -13,22 +13,23 @@ namespace LegoController
     class MainBrick
     {
         private Brick Ev3Brick;
-        private int _pickUp, _dropOff;
-        private int Turn, Forward;
-        private uint Time;
+        private int _forward, _backward, _pickUp, _dropOff;
+        private uint _time = 2000;
         private bool StopS = false;
+        private int CurrentColor = 20;
 
         public MainBrick()
         {
+            _forward = 40;
+            _backward = -30;
         }
 
         public MainBrick(int A, string SerialPortName)
         {
-            Turn = -10;
-            Forward = -10;
-            _dropOff = 50;
-            _pickUp = -50;
-            Time = 30;
+            _dropOff = 40;
+            _pickUp = -40;
+            _forward = -40;
+            _backward = 30;
             if (A == 1) Ev3Brick = new Brick(new BluetoothCommunication(SerialPortName));
             else Ev3Brick = new Brick(new UsbCommunication());
             Ev3Brick.BrickChanged += Ev3Brick_BrickChanged;
@@ -45,7 +46,7 @@ namespace LegoController
 
         public async Task Connect_Brick()
         {
-            await Ev3Brick.DirectCommand.SelectFontAsync(FontType.Medium);
+            //await Ev3Brick.DirectCommand.SelectFontAsync(FontType.Medium);
             Ev3Brick.Ports[InputPort.Two].SetMode(ColorMode.Color);
             await Ev3Brick.ConnectAsync();
             await Ev3Brick.DirectCommand.PlayToneAsync(50, 1000, 300);
@@ -69,8 +70,8 @@ namespace LegoController
         {
             switch (Mode)
             {
-                case 1: await Ev3Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.C | OutputPort.B, power, time, EmergencyBrake); break;
-                case 2: await Ev3Brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.C | OutputPort.B, power); break;
+                case 1: await Ev3Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.C | OutputPort.B, -power, time, EmergencyBrake); break;
+                case 2: await Ev3Brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.C | OutputPort.B, -power); break;
             }
         }
 
@@ -78,22 +79,22 @@ namespace LegoController
         {
             switch (Mode)
             {
-                case 1: await Ev3Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.C | OutputPort.B, -power, time, EmergencyBrake); break;
-                case 2: await Ev3Brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.C | OutputPort.B, -power); break;
+                case 1: await Ev3Brick.DirectCommand.TurnMotorAtPowerForTimeAsync(OutputPort.C | OutputPort.B, power, time, EmergencyBrake); break;
+                case 2: await Ev3Brick.DirectCommand.TurnMotorAtPowerAsync(OutputPort.C | OutputPort.B, power); break;
             }
         }
 
         public async Task TurnRight(bool EmergencyBrake, int power, uint time)
         {
-            Ev3Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, -power, time, EmergencyBrake);
-            Ev3Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, +power, time, EmergencyBrake);
+            Ev3Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, +power, time, EmergencyBrake);
+            Ev3Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, -power, time, EmergencyBrake);
             await Ev3Brick.BatchCommand.SendCommandAsync();
         }
 
         public async Task TurnLeft(bool EmergencyBrake, int power, uint time)
         {
-            Ev3Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, +power, time, EmergencyBrake);
-            Ev3Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, -power, time, EmergencyBrake);
+            Ev3Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.C, -power, time, EmergencyBrake);
+            Ev3Brick.BatchCommand.TurnMotorAtPowerForTime(OutputPort.B, +power, time, EmergencyBrake);
             await Ev3Brick.BatchCommand.SendCommandAsync();
         }
 
@@ -105,70 +106,95 @@ namespace LegoController
 
         public async Task FollowPath()
         {
-            bool CheckPickUp = false;
+            bool CheckPickUp = true;
             int PreviousColor = 20;
-            int CurrentColor = 20;
             int cnt = 0;
             StopS = false;
-            int SensorDistance = (int)Ev3Brick.Ports[InputPort.One].SIValue;
+            int SensorDistance = (int)Ev3Brick.Ports[InputPort.Three].SIValue;
             while (CurrentColor != 0 && !StopS)
             {
+                SensorDistance = (int)Ev3Brick.Ports[InputPort.Three].SIValue;
+                if (SensorDistance < 30) break;
                 CurrentColor = (int)Ev3Brick.Ports[InputPort.Two].SIValue;
                 if (CurrentColor == 6) cnt++;
                 else cnt = 0;
+                Debug.WriteLine("Current color = {0}", CurrentColor);
                 if (CurrentColor == 7 || CurrentColor == 1)
                 {
-                    await MoveForwad(true, 20, 30, 1);
                     Debug.WriteLine("Moving forward");
-                    System.Threading.Thread.Sleep(31);
+                    await MoveForwad(true, 20, 30, 1);
                     PreviousColor = 1;
-
+                    System.Threading.Thread.Sleep(32);
                 }
+                else
                 if (CurrentColor == 4 || (CurrentColor == 6 && PreviousColor == 4))
                 {
-                    Debug.WriteLine("Turning right");
+                    Debug.WriteLine("Turn right");
                     await TurnRight(true, 17, 30);
-                    System.Threading.Thread.Sleep(31);
+                    System.Threading.Thread.Sleep(32);
                     PreviousColor = 4;
                 }
+                else
                 if (CurrentColor == 5 || (CurrentColor == 6 && PreviousColor == 5))
                 {
-                    Debug.WriteLine("Turning left");
+                    Debug.WriteLine("Turn Left");
                     await TurnLeft(true, 17, 30);
-                    System.Threading.Thread.Sleep(31);
+                    System.Threading.Thread.Sleep(32);
                     PreviousColor = 5;
                 }
-                if (PreviousColor == 1 && CurrentColor == 6 || cnt >= 5)
+                else if (PreviousColor == 1 && CurrentColor == 6 || cnt > 3)
                 {
-                    if (CheckPickUp)
+                    Debug.WriteLine("IN AREA");
+                    if (CheckPickUp && true)
                     {
-                        await DropOff(true);
-                        System.Threading.Thread.Sleep(1000);
-                        await TurnRight(true, 40, 1000);
+
+                        await MoveBackward(true, 30, 600, 1);
+                        await TurnLeft(true, 20, 300);
                         System.Threading.Thread.Sleep(1500);
-                        await MoveForwad(true, 40, 1000, 1);
+                        await DropOff(true);
+                        System.Threading.Thread.Sleep(1500);
+                        await MoveBackward(true, 30, 600, 1);
+                        System.Threading.Thread.Sleep(1500);
+                        await TurnRight(true, 20, 350);
+                        System.Threading.Thread.Sleep(1500);
+                        Debug.WriteLine("Turning right");
+                        await MoveForwad(true, 40, 1500, 1);
                         System.Threading.Thread.Sleep(1500);
                         CurrentColor = (int)Ev3Brick.Ports[InputPort.Two].SIValue;
-                        while (CurrentColor == 6)
+                        PreviousColor = 0;
+                        while (CurrentColor == 6 && !StopS)
                         {
-                            await TurnRight(true, 20, 100);
+                            await TurnRight(true, 10, 50);
+                            System.Threading.Thread.Sleep(12);
                             CurrentColor = (int)Ev3Brick.Ports[InputPort.Two].SIValue;
                         }
                     }
                     else
                     {
-                        await PickUp(true);
-                        System.Threading.Thread.Sleep(1000);
-                        await TurnRight(true, 40, 1000);
-                        System.Threading.Thread.Sleep(1500);
-                        await MoveForwad(true, 40, 1000, 1);
-                        System.Threading.Thread.Sleep(1500);
-                        CheckPickUp = true;
-                        CurrentColor = (int)Ev3Brick.Ports[InputPort.Two].SIValue;
-                        while (CurrentColor == 6)
+                        SensorDistance = (int)Ev3Brick.Ports[InputPort.One].SIValue;
+                        if (SensorDistance > 7)
                         {
-                            await TurnRight(true, Turn, Time);
+
+                        }
+                        else
+                        {
+                            await PickUp(true);
+                            CheckPickUp = true;
+                            System.Threading.Thread.Sleep(1000);
+                            Debug.WriteLine("picking up");
+                            await TurnRight(true, 15, 1000);
+                            System.Threading.Thread.Sleep(1500);
+                            Debug.WriteLine("Turning right");
+                            await MoveForwad(true, 35, 1000, 1);
+                            System.Threading.Thread.Sleep(1000);
                             CurrentColor = (int)Ev3Brick.Ports[InputPort.Two].SIValue;
+                            PreviousColor = 0;
+                            while (CurrentColor == 6 && !StopS)
+                            {
+                                await TurnRight(true, 10, 50);
+                                System.Threading.Thread.Sleep(12);
+                                CurrentColor = (int)Ev3Brick.Ports[InputPort.Two].SIValue;
+                            }
                         }
                     }
                 }
@@ -186,6 +212,11 @@ namespace LegoController
 
         private void Ev3Brick_BrickChanged(object sender, BrickChangedEventArgs e)
         {
+            //Debug.WriteLine("Current color = {0}", CC);
+            //int SensorDistance = (int)Ev3Brick.Ports[InputPort.Three].SIValue;
+            // Debug.WriteLine("Distance = {0}", SensorDistance);
+            // if (SensorDistance < 200);
         }
     }
+
 }
